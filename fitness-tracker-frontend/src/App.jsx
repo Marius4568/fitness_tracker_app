2dcc2e1b3c81888c5530f4ee14c0c792caaae676
Marius Urbonas <mariolee303@gmail.com>
2025-11-02 23:08:38 +0200

    test2

--- fitness-tracker-frontend/src/App.jsx
+++ fitness-tracker-frontend/src/App.jsx
@@ -105,7 +105,7 @@ function AppContent() {
       <header>
         <div className="header-content">
           <div>
-            <h1>💪 Exercise Tracker</h1>
+            <h1>💪 Exercise Trackerrr</h1>
             <p>Welcome, {user.username}!</p>
           </div>
           <button onClick={logout} className="btn-logout">
